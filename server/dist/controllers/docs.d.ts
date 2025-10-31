9084fb41308b2b1b02455f0fb0e022c000bbc73f
Njenga Ian Josh <joshnjenga066@gmail.com>
2025-10-31 11:10:06 +0300

    Changes the server entry in the build

--- server/dist/controllers/docs.d.ts
+++ server/dist/controllers/docs.d.ts
@@ -1,4 +1,4 @@
-import { type Request, type Response } from "express";
+import type { Request, Response } from "express";
 type ApiResponse<T = unknown> = {
     success: boolean;
     message?: string;
